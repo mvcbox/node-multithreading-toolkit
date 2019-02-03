b5828832f153068cacb0f52e0c63da9597a75c1d
mvcbox <mvcbox.org@gmail.com>
2019-02-03 20:28:08 +0200

    Add worker-threads-function-stub.js

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const compareVersions = require('compare-versions');
 
 if (compareVersions(process.version.replace('v', ''), '10.5.0') >= 0) {
     exports.workerThreadsFunction = require('./worker-threads-function/index');
+} else {
+    exports.workerThreadsFunction = require('./worker-threads-function-stub');
 }
 
+exports.forkFunction = require('./fork-function/index');
---
+++ worker-threads-function-stub.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ *
+ */
+module.exports = function () {
+    throw new Error('Not supported for current Node.js version [' + process.version + ']. This functionality is supported for version v10.5.0 and higher.');
+};
