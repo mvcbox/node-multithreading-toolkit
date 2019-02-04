0bc38dfc63c4f6367fa2075a4c62ae44d8efd832
mvcbox <mvcbox.org@gmail.com>
2019-02-04 10:52:23 +0200

    Update to 1.0.3

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const compareVersions = require('compare-versions');
 
-if (compareVersions(process.version.replace('v', ''), '10.5.0') >= 0) {
-    exports.workerThreadsFunction = require('./worker-threads-function/index');
+if (compareVersions(process.version.match(/[\d\.]+/)[0], '10.5.0') >= 0) {
+    exports.workerThreadsFunction = require('./worker-threads-function-factory/index');
 } else {
-    exports.workerThreadsFunction = require('./worker-threads-function-stub');
+    exports.workerThreadsFunction = require('./worker-threads-function-factory-stub');
 }
 
-exports.forkFunction = require('./fork-function/index');
+exports.forkFunction = require('./fork-function-factory/index');
